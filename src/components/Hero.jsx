e41f392d60234b8e3f341c44f25997b2a1308310
CodeGuruAmal <amalramesh916@gmail.com>
2024-07-30 22:04:00 +0530

    add: Hero Section, fix: NavBar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
       <section className="w-full h-screen absolute top-0 font-[Moderniz]">
           {/* <p className="font-[Sloth-SemiBold]">Portfolio 2024</p> */}
           {/* <h3 className="text-[4vw]">Hello, I'm Amal</h3> */}
-        <div className="absolute -bottom-3 left-1/2 -translate-x-1/2 w-full">
+        <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-full">
           <h1 className="text-[8.9vw] whitespace-nowrap overflow-hidden text-center">Frontend Developer</h1>
         </div>
       </section>
