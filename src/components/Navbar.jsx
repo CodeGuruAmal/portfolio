c654e3c7505a376da004f2d0fadc0c91dceb40dc
CodeGuruAmal <amalramesh916@gmail.com>
2024-07-31 14:32:09 +0530

    add: Hero Section, fix: NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,7 +43,6 @@ const Navbar = () => {
       <div className="nav-container font-[Sloth-Regular]">
         <div className="navbar-top w-full relative z-10 flex justify-between items-center p-4">
           <div className="logo">
-<<<<<<< HEAD
             <svg
               width="30"
               height="35"
@@ -101,9 +100,6 @@ const Navbar = () => {
               />
             </svg>
 
-=======
-            <img className="w-8" alt="" srcSet="./assets/site-logo.svg" />
->>>>>>> c89c8f1c2403fea99ea73505d5fd2c18ea48fea5
             {/* <h1 className="px-4 py-2 text-primary font-[Moderniz]">Amal</h1> */}
           </div>
 
